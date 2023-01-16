0f79e8a3c8a8cb85fd257036de9e9797d3922046
luigi massa <lmassa@bwlab.it>
2023-01-16 22:50:39 +0100

    fix autoload markeplace

--- classes/shoppygo/MarketplaceAutoload.php
+++ classes/shoppygo/MarketplaceAutoload.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright since 2022 Bwlab of Luigi Massa and Contributors
  * Bwlab of Luigi Massa is an Italy Company
@@ -25,6 +26,9 @@
  */
 class MarketplaceAutoload extends PrestaShopAutoload
 {
+    private $front_controller_shoppygo = 'controllers/front/shoppygo/marketplace/';
+    private $classes_shoppygo_marketplace = 'classes/shoppygo/marketplace/';
+
     public function generateIndex()
     {
         if (class_exists('Configuration') && defined('_PS_CREATION_DATE_')) {
@@ -78,10 +82,10 @@ class MarketplaceAutoload extends PrestaShopAutoload
                 }
             }
         }
-
         ksort($classes);
+        $classes = $this->replaceControllerCore($classes);
 
-        $content = '<?php return '.var_export($this->replaceControllerCore($classes), true).'; ?>';
+        $content = '<?php return '.var_export($classes, true).'; ?>';
 
         // Write classes index on disc to cache it
         $filename = static::getCacheFileIndex();
@@ -104,10 +108,124 @@ class MarketplaceAutoload extends PrestaShopAutoload
         $this->index = $classes;
     }
 
+    public function load($className)
+    {
+        // Retrocompatibility
+        if (isset(static::$class_aliases[$className]) && !interface_exists($className, false) &&
+            !class_exists($className, false)) {
+            return $this->getEval($className);
+        }
+
+        // regenerate the class index if the requested file doesn't exists
+        if ((isset($this->index[$className]) && $this->index[$className]['path'] &&
+                !is_file($this->root_dir.$this->index[$className]['path'])) ||
+            (isset($this->index[$className.'Core']) && $this->index[$className.'Core']['path'] &&
+                !is_file($this->root_dir.$this->index[$className.'Core']['path'])) ||
+            !file_exists(static::getNamespacedStubFileIndex())) {
+            $this->generateIndex();
+        }
+
+        // If $classname has not core suffix (E.g. Shop, Product)
+        if (substr($className, -4) != 'Core' && !class_exists($className, false)) {
+            $classDir = (isset($this->index[$className]['override']) &&
+                $this->index[$className]['override'] === true) ? $this->normalizeDirectory(
+                _PS_ROOT_DIR_
+            ) : $this->root_dir;
+
+            // If requested class does not exist, load associated core class
+            if (isset($this->index[$className]) && !$this->index[$className]['path']) {
+                require_once $classDir.$this->index[$className.'Core']['path'];
+                $this->loadMarketplaceCoreClass($className, $classDir);
+
+                if ($this->index[$className.'Core']['type'] != 'interface') {
+                    $marketplacePrefixClass = $this->getMarketplacePrefixClass($className);
+                    $this->getEvalCore($className, $marketplacePrefixClass);
+                }
+            } else {
+                // request a non Core Class load the associated Core class if exists
+                if (isset($this->index[$className.'Core'])) {
+                    require_once $this->root_dir.$this->index[$className.'Core']['path'];
+                }
+
+                if (isset($this->index[$className])) {
+                    require_once $classDir.$this->index[$className]['path'];
+                }
+            }
+        } elseif (isset($this->index[$className]['path']) && $this->index[$className]['path']) {
+            // Call directly ProductCore, ShopCore class
+            require_once $this->root_dir.$this->index[$className]['path'];
+        }
+        if (strpos($className, 'PrestaShop\PrestaShop\Adapter\Entity') !== false) {
+            require_once static::getNamespacedStubFileIndex();
+        }
+    }
+
+    private function getEval(string $className): mixed
+    {
+        return eval('class '.$className.' extends '.static::$class_aliases[$className].' {}');
+    }
+
+    private function getEvalCore(string $className, string $marketplacePrefixClass): void
+    {
+        eval(
+            $this->index[$className.'Core']['type'].' '.$className.' extends '.$marketplacePrefixClass.$className.
+            'Core {}'
+        );
+    }
+
+    private function getMarketplacePrefixClass(string $className): string
+    {
+        $marketplacePrefixClass = '';
+        if (isset($this->index[$className.'Core']['marketplace']) === true &&
+            $this->index[$className.'Core']['marketplace'] === true) {
+            $marketplacePrefixClass = '\Marketplace';
+        }
+
+        return $marketplacePrefixClass;
+    }
+
+    private function loadMarketplaceCoreClass(string $className, string $classDir): void
+    {
+        if ($this->isMarketplaceClass($className) === true) {
+            if ($this->isControllerClass($className) === true) {
+                require_once $classDir.$this->front_controller_shoppygo.'Marketplace'.$className.'.php';
+            } else {
+                require_once $classDir.$this->classes_shoppygo_marketplace.'Marketplace'.$className.'.php';
+            }
+        }
+    }
+
+    private function isControllerClass($classname)
+    {
+        if (isset($this->index[$classname.'Core']['controller']) === false) {
+            return false;
+        }
+
+        return $this->index[$classname.'Core']['controller'] === true;
+    }
+
+    private function isMarketplaceClass($classname)
+    {
+        if (isset($this->index[$classname.'Core']['marketplace']) === false) {
+            return false;
+        }
+
+        return $this->index[$classname.'Core']['marketplace'] === true;
+    }
+
+    private function normalizeDirectory($directory)
+    {
+        return rtrim($directory, '/\\').DIRECTORY_SEPARATOR;
+    }
+
     private function replaceControllerCore(array $classes): array
     {
-        $classes['HistoryControllerCore']['path'] = 'controllers/front/shoppygo/marketplace/MarketplaceHistoryController.php';
-        $classes['OrderCore']['path'] = 'classes/shoppygo/markeplace/MarketplaceOrderCore.php';
+        $classes['HistoryControllerCore']['marketplace'] = true;
+        $classes['HistoryControllerCore']['controller'] = true;
+
+        $classes['OrderCore']['marketplace'] = true;
+        $classes['OrderCore']['controller'] = false;
+
         return $classes;
     }
 }
